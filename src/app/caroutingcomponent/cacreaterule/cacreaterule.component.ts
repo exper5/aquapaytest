56a68e31fdfd78b91a4ac89d3c508227ce67d3da
AniketDG <aniketgulwade@gmail.com>
2018-06-28 23:29:20 +0530

    update

--- src/app/caroutingcomponent/cacreaterule/cacreaterule.component.ts
+++ src/app/caroutingcomponent/cacreaterule/cacreaterule.component.ts
@@ -26,16 +26,27 @@ $(document).ready(function (e) {
     });
 });
 
-$(document).ready(function (e) {
-  $('#firstgrp .dropdown-menu').find('a').click(function (e) {
+function a(){
+  $(document).ready(function (e) {
+    // $('.firstgrp .dropdown-menu').find('a').click(function (e) {
+    
+    //       e.preventDefault();
+    //       //var param = $(this).attr("href").replace("#", "");
+    //       var concept = $(this).text();
+    //       $('.firstgrp span#search_concept').text(concept);
+    //      // $('.input-group #search_param').val(param);
+    // });
+    $('.firstgrp .dropdown-menu').find('a').click(function (e) {
+    
       e.preventDefault();
-      var param = $(this).attr("href").replace("#", "");
+      //var param = $(this).attr("href").replace("#", "");
       var concept = $(this).text();
-      $('#firstgrp span#search_concept').text(concept);
-      $('.input-group #search_param').val(param);
-  });
+      $('.firstgrp span#search_concept').text(concept);
+     // $('.input-group #search_param').val(param);
 });
-
+  });
+}
+a();
 $(document).ready(function (e) {
   $('#secondgrp .dropdown-menu').find('a').click(function (e) {
       e.preventDefault();
@@ -47,68 +58,70 @@ $(document).ready(function (e) {
 });
 
 //----------------------------drop down end---------------------------------------
-
-
-//------------------------------add new row strt-------------------------------------------
 $(document).ready(function(){
-  $(".add-moree").click(function(){ 
-    var html = $(".copyy").html();
-    $(".after-add-moree").after(html);
+  $(".add-more").click(function(){
+    var html = $(this).closest(".input_fields_wrap");
+    var seccount= $(".custBtnWrap").length;
+    if(seccount <= 3) {
+
+      html.clone().appendTo(".copyyyy");
+      $(".copyyyy").find(".add-more").attr("minus","true");
+      $(".copyyyy").find(".add-more").text("-");
+  
+      $("[minus=true]").click(function(){ 
+      $(this).closest(".input_fields_wrap").remove();
+      a();
+    });
+    }
+
+   
+    
 });
+
+
 
 
 $("body").on("click",".remove",function(){ 
     $(this).parents(".control-group").remove();
 });
-
-
-$("#flip").click(function(){
-  $("#panel").slideToggle("slow");
-});
-$("#flip-sup").click(function(){
-  $("#panel-sup").slideToggle("slow");
-});
-
-});
-
-
-
-$(document).ready(function(){			
-  var opdcount = 1;
-      $("button[id^='add_div']").click(function(){
-      var attri=$(this).attr('class');
-      opdcount++;
-      var opdnewclass=attri.replace('add','append');
-      var opdtr = "opdtr" + opdcount;
-  
-      var tbodyopd = ("#new_id");
-      var addnewclass=attri.replace('append','add');
-      var add="add" + addnewclass;
-      var op = $("<div id="+ opdtr +" ><div class='control-group input-group' style='margin-top:36px'><input type='email' name='addmore[]' class='input-text'> <label class='label' for='email' id='email_count'>Supplier Email "+ opdcount +"</label><div class='input-group-btn bordr'> <button class='btn remove' type='button'>-</button></div></div></div>");
-      op.appendTo(tbodyopd);
-      
-  });
-});
-var opdcount_sup = 1;
-                              $("button[id^='add_div_sup']").click(function(){
-            var attri=$(this).attr('class');
-            opdcount_sup++;
-            var opdnewclass_sup=attri.replace('add_sup','append_sup');
-            var opdtr_sup = "opdtr_sup" + opdcount_sup;
-            
-            var tbodyopd_sup = ("#new_id_sup");
-            var addnewclass_sup=attri.replace('append_sup','add_sup');
-            var add="add_sup" + addnewclass_sup;
-            var op_sup = $("<div id="+ opdtr_sup +" ><div class='control-group input-group' style='margin-top:36px'><input type='email' name='addmore[]' class='input-text'> <label class='label' for='email' id='email_count'>Supplier Contact "+ opdcount_sup +"</label><div class='input-group-btn bordr'> <button class='btn remove' type='button'>-</button></div></div></div>");
-            op_sup.appendTo(tbodyopd_sup);
-            op_sup.appendTo("");
-          });
-$("body").on("click",".remove",function(){ 
+ });
+ $("body").on("click",".remove",function(){ 
 $(this).parents(".control-group").remove();
 });
 
 
-//-------------------------------add new row stop----------------------------------------------
+//---------------------------------------check box js -------------------------------------------
+    
+$(".place").click(function () {
+  $(this).toggleClass("green");
+});
+//---------------------------------------check box js edn-------------------------------------------
+
+//----------------------------------table js--togglr----------------------------------------------
+$(".rowShow").hide();
+
+$('.showhide').click(function(){
+  //alert("clicked");
+  $(this).toggleClass('flashOn flash');
+  var row=$(this).parent().parent().next();
+  $(row).toggle();
+ // $(row).next().toggle();
+})
+
+//-----------------table js toggle end --------------------------------------------------------------------
+$(document).ready(function(){
+  $("#step-2").hide();
+  $("#step-3").hide();
+  $(".createrule").click(function(){
+  $("#step-2").show();
+  });
+});
+$(".Approve").click(function(){
+  $("#main1").hide();
+  $("#step-3").show();
+  
+  });
+
   }
 
 }
